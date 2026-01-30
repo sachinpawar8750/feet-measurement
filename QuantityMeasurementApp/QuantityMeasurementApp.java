6388123f2eef02a4d47ecc2382f382db0cafdde0
sachin <sachinpawar8750@gmail.com>
2026-01-30 11:24:54 +0530

    changes for inches measurement without test cases

--- QuantityMeasurementApp/QuantityMeasurementApp.java
+++ QuantityMeasurementApp/QuantityMeasurementApp.java
@@ -23,6 +23,31 @@ public class QuantityMeasurementApp {
         }
     }
 
+    // ===== Inches Class =====
+    static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            if (Double.isNaN(value)) {
+                throw new IllegalArgumentException("Invalid inches value");
+            }
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+
     // ===== Static method for Feet equality =====
     public static boolean checkFeetEquality(double v1, double v2) {
         Feet f1 = new Feet(v1);
@@ -30,6 +55,12 @@ public class QuantityMeasurementApp {
         return f1.equals(f2);
     }
 
+    // ===== Static method for Inches equality =====
+    public static boolean checkInchesEquality(double v1, double v2) {
+        Inches i1 = new Inches(v1);
+        Inches i2 = new Inches(v2);
+        return i1.equals(i2);
+    }
 
     // ===== Main Method =====
     public static void main(String[] args) {
@@ -38,9 +69,13 @@ public class QuantityMeasurementApp {
         boolean feetResult = checkFeetEquality(5.0, 5.0);
         boolean feetResult1 = checkFeetEquality(12, 5.0);
         boolean feetResult2 = checkFeetEquality(5.0, 15);
+        boolean inchesResult1 = checkInchesEquality(12.0, 12.0);
+        boolean inchesResult2 = checkInchesEquality(10.0, 12.0);
 
         System.out.println("Feet equality result: " + feetResult);
         System.out.println("Feet equality result: " + feetResult1);
         System.out.println("Feet equality result: " + feetResult2);
+        System.out.println("Inches equality (same): " + inchesResult1);
+        System.out.println("Inches equality (different): " + inchesResult2);
     }
 }
