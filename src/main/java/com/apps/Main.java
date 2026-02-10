43b84f6aff4b9656bebef23cc75e4249e59bc1c2
Sachin Pawar <sachinpawar8750@gmail.com>
2026-02-10 22:56:23 +0530

    removed unwated files and folders

--- src/main/java/com/apps/Main.java
+++ src/main/java/com/apps/Main.java
@@ -1,17 +1,21 @@
 package com.apps;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");:
+import com.apps.measurement.util.MeasurementComparator;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean feetResult1 = MeasurementComparator.compareFeet(10, 10);
+        boolean feetResult2 = MeasurementComparator.compareFeet(17, 20);
+
+        boolean inchResult1 = MeasurementComparator.compareInches(2, 2);
+        boolean inchResult2 = MeasurementComparator.compareInches(3, 3);
+
+        System.out.println(feetResult1);
+        System.out.println(feetResult2);
+        System.out.println("***************");
+        System.out.println(inchResult1);
+        System.out.println(inchResult2);
     }
 }
